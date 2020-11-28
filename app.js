8137c938638065bac199fa523de17796ea84d1b6
David Pease <david.f.pease@gmail.com>
2020-11-28 13:14:57 -0800

    Add database connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello Hello"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
 const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
